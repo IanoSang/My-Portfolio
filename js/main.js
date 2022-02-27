f1ea8ad800817a9833451d95c7150a290eb65d29
IanoSang <ianosang18@gmail.com>
2022-02-27 21:52:13 +0300

    feat: home page- adds content to the page

--- js/main.js
+++ js/main.js
@@ -1,106 +1,49 @@
-/*global jQuery:false */
-jQuery(document).ready(function($) {
-    "use strict";
+let sliderImages = document.querySelectorAll(".slide"),
+    arrowLeft = document.querySelector("#arrow-left"),
+    arrowRight = document.querySelector("#arrow-right"),
+    current = 0;
 
-    //Contact
-    $('form.validateform').submit(function(){
+// Clear all images
+function reset() {
+    for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = "none";
+    }
+}
 
-        var f = $(this).find('.field'),
-            ferror = false,
-            emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+// Initial slide
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = "block";
+}
 
-        f.children('input').each(function(){ // run all inputs
+// Show previous
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = "block";
+    current--;
+}
 
-            var i = $(this); // current input
-            var rule = i.attr('data-rule');
+// Show next
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = "block";
+    current++;
+}
 
-            if( rule != undefined ){
-                var ierror=false; // error flag for current input
-                var pos = rule.indexOf( ':', 0 );
-                if( pos >= 0 ){
-                    var exp = rule.substr( pos+1, rule.length );
-                    rule = rule.substr(0, pos);
-                }else{
-                    rule = rule.substr( pos+1, rule.length );
-                }
-
-                switch( rule ){
-                    case 'required':
-                        if( i.val()=='' ){ ferror=ierror=true; }
-                        break;
-
-                    case 'maxlen':
-                        if( i.val().length<parseInt(exp) ){ ferror=ierror=true; }
-                        break;
-
-                    case 'email':
-                        if( !emailExp.test(i.val()) ){ ferror=ierror=true; }
-                        break;
-
-
-                    case 'checked':
-                        if( !i.attr('checked') ){ ferror=ierror=true; }
-                        break;
-
-                    case 'regexp':
-                        exp = new RegExp(exp);
-                        if( !exp.test(i.val()) ){ ferror=ierror=true; }
-                        break;
-                }
-                i.next('.validation').html( ( ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '' ) ).show('blind');
-            }
-        });
-        f.children('textarea').each(function(){ // run all inputs
-
-            var i = $(this); // current input
-            var rule = i.attr('data-rule');
-
-            if( rule != undefined ){
-                var ierror=false; // error flag for current input
-                var pos = rule.indexOf( ':', 0 );
-                if( pos >= 0 ){
-                    var exp = rule.substr( pos+1, rule.length );
-                    rule = rule.substr(0, pos);
-                }else{
-                    rule = rule.substr( pos+1, rule.length );
-                }
-
-                switch( rule ){
-                    case 'required':
-                        if( i.val()=='' ){ ferror=ierror=true; }
-                        break;
-
-                    case 'maxlen':
-                        if( i.val().length<parseInt(exp) ){ ferror=ierror=true; }
-                        break;
-                }
-                i.next('.validation').html( ( ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '' ) ).show('blind');
-            }
-        });
-        if( ferror ) return false;
-        else var str = $(this).serialize();
-
-        $.ajax({
-            type: "POST",
-            url: "#contact/index.html",
-            data: str,
-            success: function(msg){
-                $("#sendmessage").addClass("show");
-                $("#errormessage").ajaxComplete(function(event, request, settings){
-
-                    if(msg == 'OK')
-                    {
-                        $("#sendmessage").addClass("show");
-
-                    }
-                    else
-                    {
-                        $("#sendmessage").removeClass("show");
-                        result = msg;
-                    }
-
-                    $(this).html(result);});}});
-        return false;
-    });
-
+// Left arrow click
+arrowLeft.addEventListener("click", function () {
+    if (current === 0) {
+        current = sliderImages.length;
+    }
+    slideLeft();
 });
+
+// Right arrow click
+arrowRight.addEventListener("click", function () {
+    if (current === sliderImages.length - 1) {
+        current = -1;
+    }
+    slideRight();
+});
+
+startSlide();
